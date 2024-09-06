ea42958582e9cae0a731b5341ece366000b48eae
GlamorousBrusier12 <pandurebelsachin18@gmail.com>
2024-09-07 01:50:03 +0530

    updated number

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,5 +3,5 @@ import { useBalance } from "@repo/store/useBalance";
 
 export default function Home() {
   const balance = useBalance();
-  return <div className="text-2xl">hi there {balance.toString()}</div>;
+  return <div className="text-2xl">hi there {Number(balance)}</div>;
 }
