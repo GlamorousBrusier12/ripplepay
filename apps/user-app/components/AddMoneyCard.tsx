bb8f195ecd8b2c1180a825f71fcf3b7b1e0f3a64
GlamorousBrusier12 <pandurebelsachin18@gmail.com>
2024-09-09 23:08:08 +0530

    removed unused varibles

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -4,7 +4,6 @@ import { Card } from "@repo/ui/card";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/textinput";
-import { Provider } from "../provider";
 import { createOnRampTransaction } from "../app/lib/actions/createOnrampTransaction";
 
 const SUPPORTED_BANKS = [
