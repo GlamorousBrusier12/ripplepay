276bb40296c78c8de99a70a9a35089070ed4cecf
GlamorousBrusier12 <pandurebelsachin18@gmail.com>
2024-09-09 22:58:29 +0530

    updated fetch users

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -2,7 +2,7 @@ import prisma from "@repo/db/client";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const users = await prisma.user.findMany({});
+  const users: (typeof prisma.user)[] = [];
 
   return NextResponse.json({
     message: "hello!",
